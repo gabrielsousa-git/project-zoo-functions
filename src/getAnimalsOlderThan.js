798cbdbd6b41688828100a486fa863741be0447f
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-06-30 17:52:52 -0300

    Requisito 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.every((specie) => specie.name === animal && specie.residents.age >= age);
 }
 
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find((e) => e.firstName === employeeName || e.lastName === employeeName);
 }
-
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
