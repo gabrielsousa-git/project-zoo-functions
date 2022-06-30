7632a5e64045275ecfee2d5df1bff56eb99f6206
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-06-30 06:40:56 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter((animal, index) => animal.id === ids[0] || animal.id === ids[index]);
 }
 
 module.exports = getSpeciesByIds;
