caecc3da30de16bfe6a06e2e9e48de7fe3ca23a1
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-07-01 15:05:01 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -16,5 +16,5 @@ function getRelatedEmployees(managerId) {
     return [emp1, emp2, emp3];
   } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
