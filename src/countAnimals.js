b285bad0449515a161e101d015fc6de81ead81a0
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-07-02 07:36:28 -0300

    Requisitos 6 e 7

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const empty = {};
+  if (!animal) {
+    species.forEach((specie) => { empty[specie.name] = specie.residents.length; });
+    return empty;
+  }
+  const count = {};
+  if (species.name === animal) {
+    species.forEach((specie) => { count[specie.name] = specie.residents.length; });
+    return count;
+  }
 }
-
+console.log(countAnimals('lions'));
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,31 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Retorna a qauntidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Retorna os nomes dos elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('Retorna a a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('Retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('Se o parâmetro é undefined retorna undefined', () => {
+    expect(handlerElephants(undefined)).toBe(undefined);
+  });
+  it('Testa se o parâmetro passado é uma string', () => {
+    expect(handlerElephants(5)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Se o parâmetro passado não for válido retorna null', () => {
+    expect(handlerElephants('testeTeste')).toBe(null);
+  });
+});
