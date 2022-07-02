39ba49efdca6eb23d755095076c73de500e3e533
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-07-02 10:13:20 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,8 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return species.every((specie) => specie.name === animal && specie.residents.age >= age);
+  return species.find((sp) => sp.name === animal).residents.every((sp) => sp.age >= age);
 }
 
-console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
