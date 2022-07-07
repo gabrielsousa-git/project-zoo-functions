43e32aeecf7b7a3bf249fa441591ea9cf37a6681
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-07-07 08:50:46 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const person = employees.find((employee) => employee.id === id).responsibleFor.find((sp) => sp);
+  const animal = species.find((specie) => specie.id === person).residents;
+  return Object.values(animal.sort(({ age: ageA }, { age: ageB }) => ageB - ageA)[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
