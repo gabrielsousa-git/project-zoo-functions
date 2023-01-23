2d1b1cb1329a0fd813fb1489d9c18d1c33dbc86c
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2023-01-23 16:46:31 -0300

    Finalizando 100%

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const structure = data.employees.map((elementSpecie) => (
+  {
+    id: elementSpecie.id,
+    fullName: `${elementSpecie.firstName} ${elementSpecie.lastName}`,
+    species: elementSpecie.responsibleFor.map((elementId) => (
+      data.species.find((element1) => (element1.id === elementId)).name
+    )),
+    locations: elementSpecie.responsibleFor.map((elementId) => (
+      data.species.find((element2) => (element2.id === elementId)).location
+    )),
+  }
+));
+
+function getEmployeesCoverage(nameOrId) {
+  const condition = nameOrId && structure.find((element) => element.id === nameOrId.id
+  || element.fullName.split(' ')[0] === nameOrId.name
+  || element.fullName.split(' ')[1] === nameOrId.name);
+  if (condition) {
+    return condition;
+  }
+  if (nameOrId) {
+    throw new Error('Informações inválidas');
+  }
+  return structure;
 }
 
 module.exports = getEmployeesCoverage;
